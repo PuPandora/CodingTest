4a7d893ff08ada30cfe31fd28c894b78544bca9f
PuPandora <pandora6722@gmail.com>
2024-01-03 10:55:16 +0900

    [LV 0] 배열의 평균값

---
+++ Lv0/120817. 배열의 평균값/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    public static double Solution(int[] numbers)
+    {
+        double result = 0;
+        foreach (int n in numbers)
+        {
+            result += n;
+        }
+        result /= numbers.Length;
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+    }
+}
