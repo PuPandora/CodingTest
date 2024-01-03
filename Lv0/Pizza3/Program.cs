e8f5be913bb7f4fb5f37283fbfe8e7d94c5fc770
PuPandora <pandora6722@gmail.com>
2024-01-03 10:25:20 +0900

    [LV 0] 피자 나눠 먹기 (3)

---
+++ Lv0/Pizza3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    /// <param name="slice">피자 조각 수</param>
+    /// <param name="n">인원 수</param>
+    public static int Solution(int slice, int n)
+    {
+        // 피자 조각 수가 충분한 경우
+        if (slice > n)
+        {
+            return 1;
+        }
+        else
+        {
+            // 나머지가 없는 경우
+            if (n % slice == 0)
+            {
+                return n / slice;
+            }
+            else
+            {
+                double result = Math.Ceiling((double)n / slice);
+                return (int)result;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Solution(7, 10));
+    }
+}
