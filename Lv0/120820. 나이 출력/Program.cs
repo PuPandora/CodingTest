c32cba01bdb9ecc9590d67556c337d6157ceee78
PuPandora <pandora6722@gmail.com>
2024-01-04 13:48:40 +0900

    [LV 0] 120820. 나이 출력

---
+++ Lv0/120820. 나이 출력/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAge
+{
+    internal class Program
+    {
+        static int Solution(int age)
+        {
+            int year = 2022;
+            return year - age + 1;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
