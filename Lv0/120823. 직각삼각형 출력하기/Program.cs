f4d2f3593bd5976b92156d962ca254693b4c5bd7
PuPandora <pandora6722@gmail.com>
2024-01-04 14:31:10 +0900

    [LV 0] 120823. 직각삼각형 출력하기

---
+++ Lv0/120823. 직각삼각형 출력하기/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int n = int.Parse(s[0]);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
