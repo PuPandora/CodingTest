63a4c32c7ba9df34ac9970b35e6dad4ce6f4c287
PuPandora <pandora6722@gmail.com>
2024-01-04 14:02:20 +0900

    [LV 0] 120821. 배열 뒤집기

---
+++ Lv0/120821. 배열 뒤집기/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static int[] Solution(int[] num_list)
+    {
+        int[] answer = new int[num_list.Length];
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            answer[i] = num_list[num_list.Length - i - 1];
+        }
+        return answer;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8});
+    }
+}
