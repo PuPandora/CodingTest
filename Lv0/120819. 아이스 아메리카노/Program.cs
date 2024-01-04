5591e0b73a70a9d75bf4d42016dec9116f2a5754
PuPandora <pandora6722@gmail.com>
2024-01-04 13:19:06 +0900

    [LV 0] 120819. 아이스 아메리카노

---
+++ Lv0/120819. 아이스 아메리카노/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static int[] Solution(int money)
+    {
+        const int coffeePrice = 5500;
+        return new int[2] { money / coffeePrice, money % coffeePrice };
+    }
+}
