f3dd8a77f07ca6b59aaad8435c56be782455b39f
PuPandora <pandora6722@gmail.com>
2024-04-21 03:41:14 +0900

    [LV0] 181941. 문자 리스트를 문자열로 반환하기
    프로그래머스 181941. 문자 리스트를 문자열로 반환하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181941

---
+++ Lv0/181941. 문자 리스트를 문자열로 변환하기/ConvertToString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 181941. 문자 리스트를 문자열로 반환하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181941
+
+public class Solution
+{
+    public string solution(string[] arr)
+    {
+        string answer = string.Empty;
+
+        foreach (var c in arr)
+        {
+            answer += c;
+        }
+
+        return answer;
+    }
+}
