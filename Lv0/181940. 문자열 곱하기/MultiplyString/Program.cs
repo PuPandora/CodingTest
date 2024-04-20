215834869039204e79e1c08e9d70870f1b827e26
PuPandora <pandora6722@gmail.com>
2024-04-21 03:44:15 +0900

    [LV 0] 181940. 문자열 곱하기
    프로그래머스 181940. 문자열 곱하기
    https://school.programmers.co.kr/learn/courses/30/lessons/181940

---
+++ Lv0/181940. 문자열 곱하기/MultiplyString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 181940. 문자열 곱하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181940
+
+public class Solution
+{
+    public string solution(string my_string, int k)
+    {
+        string answer = string.Empty;
+
+        for (int i = 0; i < k; i++)
+        {
+            answer += my_string;
+        }
+
+        return answer;
+    }
+}
