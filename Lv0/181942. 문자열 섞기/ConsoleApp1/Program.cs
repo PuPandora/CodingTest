76ab507bfce205ef228527a5e721874a53193d89
PuPandora <pandora6722@gmail.com>
2024-04-21 03:36:26 +0900

    [LV 0] 181942. 문자열 섞기
    프로그래머스 181942. 문자열 섞기
    https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=csharp

---
+++ Lv0/181942. 문자열 섞기/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 181942. 문자열 섞기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181942?language=csharp
+
+public class Soluition
+{
+    public string solution(string str1, string str2)
+    {
+        string answer = string.Empty;
+
+        char[] char1 = str1.ToCharArray();
+        char[] char2 = str2.ToCharArray();
+
+        for (int i = 0; i < str1.Length; i++)
+        {
+            answer += char1[i];
+            answer += char2[i];
+        }
+
+        return answer;
+    }
+}
