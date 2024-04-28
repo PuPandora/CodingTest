3a04b6d38d440178a4b6770100c39f73dba9a9f6
PuPandora <pandora6722@gmail.com>
2024-04-28 22:30:49 +0900

    [LV 0] 120824. 짝수 홀수 개수
    프로그래머스 120824. 짝수 홀수 개수https://school.programmers.co.kr/learn/courses/30/lessons/120824

---
+++ Lv0/120824. 짝수 홀수 개수/EvenOddNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 프로그래머스 120824. 짝수 홀수 개수
+// https://school.programmers.co.kr/learn/courses/30/lessons/120824
+
+public class Solution
+{
+    public int[] solution(int[] num_list)
+    {
+        int[] answer = new int[2] { 0, 0 };
+        int evenCount = 0;
+        int oddCount = 0;
+
+        foreach (int i in num_list)
+        {
+            // 짝수일 경우
+            if (i % 2 == 0)
+            {
+                evenCount++;
+            }
+            // 홀수일 경우
+            else
+            {
+                oddCount++;
+            }
+        }
+
+        answer[0] = evenCount;
+        answer[1] = oddCount;
+
+        return answer;
+    }
+}
